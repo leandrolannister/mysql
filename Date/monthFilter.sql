436270b429e9f900eaf1a3650933651215b3a3e1
leandrolannister <leandrohendrix@gmail.com>
2021-06-21 07:09:12 -0300

    FilterDate

---
+++ Date/monthFilter.sql
@@ -0,0 +1,18 @@
+-- FILTRO POR MES --
+DROP DATABASE IF EXISTS estudos;
+CREATE DATABASE IF NOT EXISTS estudos;
+
+use estudos;
+DROP TABLE IF EXISTS pessoas;
+
+CREATE TABLE IF NOT EXISTS 
+  pessoas (
+    id int auto_increment primary key, 
+    dt_nasc date   
+);
+
+INSERT INTO pessoas(dt_nasc) VALUES('1983-01-01');
+INSERT INTO pessoas(dt_nasc) VALUES('2000-03-01');
+INSERT INTO pessoas(dt_nasc) VALUES('2001-04-01');
+
+SELECT * FROM pessoas WHERE MONTH(dt_nasc) = 3;	
