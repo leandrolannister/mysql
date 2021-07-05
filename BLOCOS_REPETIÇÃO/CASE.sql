ff5b5fbd93672c2f2573a74f369773243937261d
leandrolannister <leandrohendrix@gmail.com>
2021-07-05 08:36:04 -0300

    CASE

--- BLOCOS_REPETIÇÃO/CASE.sql
+++ BLOCOS_REPETIÇÃO/CASE.sql
@@ -1,32 +1,26 @@
 ﻿DROP DATABASE IF EXISTS estudos;
 CREATE DATABASE estudos;
+
 USE estudos;
 
-DROP PROCEDURE IF EXISTS reajuste;
+CREATE TABLE Pessoas(id int auto_increment,
+nome varchar(50),
+tipo int,
+primary key(id));
 
-DELIMITER $$
-CREATE FUNCTION reajuste(salario DEC(10,2)) RETURNS DECIMAL(10,2)
-  BEGIN 
-    DECLARE reajuste DECIMAL(10,2);
-    
-    CASE salario 
-      WHEN salario < 100 THEN
-      SET reajuste = 0.00;
-   
-      WHEN salario < 150 THEN 
-        SET reajuste = salario * 0.15;
+INSERT INTO Pessoas(nome, tipo) VALUES('Leandro', 1);
+INSERT INTO Pessoas(nome, tipo) VALUES('Soares', 2);
+INSERT INTO Pessoas(nome, tipo) VALUES('Ribeiro', 3);
+INSERT INTO Pessoas(nome, tipo) VALUES('Pimenta', 33);
 
-      ELSE 
-        SET reajuste = salario * 0.30;
-   END CASE;   
-    
-    RETURN reajuste;
-  END $$
-DELIMITER ;
-
-SET @salario = 170;
-SELECT reajuste(@salario);
-
-
-
+SELECT 
+   nome 
+   , tipo
+   , CASE 
+       WHEN tipo = 1 THEN 'Categoria C' 
+       WHEN tipo = 2 THEN 'Categoria B'
+       WHEN tipo = 3 THEN 'Categoria A'
+       ELSE 'Sem Clássificação'
+     END AS 'Class'
+ FROM Pessoas;
 
---
+++ index.sql
@@ -0,0 +1,1 @@
+blocos_reprtição->IF
